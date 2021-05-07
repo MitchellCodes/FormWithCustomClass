10986458a042cda7cc9eaa2eaf6173542ecb4b0f
MitchellCodes <82383787+MitchellCodes@users.noreply.github.com>
2021-05-06 19:28:39 -0700

    Added more validation for the form

--- js/main.js
+++ js/main.js
@@ -8,13 +8,48 @@ window.onload = function () {
     addBtn.onclick = addVideoGame;
 };
 function addVideoGame() {
+    clearAllErrors();
     if (isAllDataValid()) {
         var game = getVideoGame();
         displayGame(game);
     }
 }
+function clearAllErrors() {
+    var errorSummary = document.getElementById("validation-summary");
+    errorSummary.innerText = "";
+}
+function getInputById(id) {
+    return document.getElementById(id);
+}
 function isAllDataValid() {
-    return true;
+    var isValid = true;
+    var title = getInputById("title").value;
+    if (title == "") {
+        isValid = false;
+        addErrorMessage("Title is required.");
+    }
+    var price = getInputById("price").value;
+    var priceValue = parseFloat(price);
+    if (price == "" || isNaN(priceValue)) {
+        isValid = false;
+        addErrorMessage("Price is required and must be a number.");
+    }
+    else if (priceValue < 0) {
+        isValid = false;
+        addErrorMessage("Price cannot be negative.");
+    }
+    var rating = document.getElementById("rating").value;
+    if (rating == "") {
+        isValid = false;
+        addErrorMessage("You must choose a rating.");
+    }
+    return isValid;
+}
+function addErrorMessage(errorMessage) {
+    var errorSummary = document.getElementById("validation-summary");
+    var errorItem = document.createElement("li");
+    errorItem.innerText = errorMessage;
+    errorSummary.appendChild(errorItem);
 }
 function displayGame(game) {
     var displayDiv = document.getElementById("display");
--- ts/main.ts
+++ ts/main.ts
@@ -20,15 +20,59 @@ window.onload = function(){
 }
 
 function addVideoGame(){
+    clearAllErrors();
     if(isAllDataValid()){
-        let game = getVideoGame();
+        let game:VideoGame = getVideoGame();
         displayGame(game);
     }
 }
 
-// TODO: ADD VALIDATION************************************************************
+/**
+ * Clears all errors in validation summary
+ */
+function clearAllErrors(){
+    let errorSummary = document.getElementById("validation-summary");
+    errorSummary.innerText = "";
+}
+
+function getInputById(id:string):HTMLInputElement{
+    return <HTMLInputElement>document.getElementById(id);
+}
+
 function isAllDataValid(){
-    return true;
+    let isValid = true;
+
+    let title = getInputById("title").value;
+    if(title == ""){
+        isValid = false;
+        addErrorMessage("Title is required.");
+    }
+
+    let price = getInputById("price").value;
+    let priceValue = parseFloat(price);
+    if(price == "" || isNaN(priceValue)){
+        isValid = false;
+        addErrorMessage("Price is required and must be a number.");
+    }
+    else if(priceValue < 0){
+        isValid = false;
+        addErrorMessage("Price cannot be negative.");
+    }
+
+    let rating = (<HTMLOptionElement>document.getElementById("rating")).value;
+    if(rating == ""){
+        isValid = false;
+        addErrorMessage("You must choose a rating.")
+    }
+
+    return isValid;
+}
+
+function addErrorMessage(errorMessage:string):void{
+    let errorSummary = document.getElementById("validation-summary");
+    let errorItem = document.createElement("li");
+    errorItem.innerText = errorMessage;
+    errorSummary.appendChild(errorItem);
 }
 
 function displayGame(game:VideoGame):void{
